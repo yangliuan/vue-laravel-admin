14efecaf4fd5cec2937855a8ed885cc03f3bbff4
yangliuancn <yangliuan@foxmail.com>
2021-10-24 13:33:52 +0800

    修改

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,23 +7,17 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <div class="user-name">{{ name }}</div>
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
             <el-dropdown-item>
-              Home
+              主页
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">退出登录</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -44,7 +38,7 @@ export default {
   computed: {
     ...mapGetters([
       'sidebar',
-      'avatar'
+      'name'
     ])
   },
   methods: {
@@ -121,6 +115,13 @@ export default {
         .user-avatar {
           cursor: pointer;
           width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .user-name {
+          cursor: pointer;
+          width: auto;
           height: 40px;
           border-radius: 10px;
         }
