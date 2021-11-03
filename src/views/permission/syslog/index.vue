5534ee46e263a5ad84453f7f771726cb10216c7c
yangliuan <yangliuancn@foxmail.com>
2021-11-03 13:32:02 +0800

    修复格式

--- src/views/permission/syslog/index.vue
+++ src/views/permission/syslog/index.vue
@@ -4,82 +4,84 @@
     element-loading-text="加载中..."
     class="tableWrapper"
   >
-  <div class="search">
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <div class="search">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
 
-      <el-form-item label="开始时间">
-        <el-date-picker
-          v-model="start_at"
-          type="datetime"
-          placeholder="选择日期时间"
-          align="right"
-          :picker-options="pickerOptions">
-        </el-date-picker>
-      </el-form-item>
+        <el-form-item label="开始时间">
+          <el-date-picker
+            v-model="start_at"
+            type="datetime"
+            placeholder="选择日期时间"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            align="right"
+            :picker-options="pickerOptions"
+          />
+        </el-form-item>
 
-    <el-form-item label="结束时间">
-      <el-date-picker
-        v-model="end_at"
-        type="datetime"
-        placeholder="选择日期时间"
-        align="right"
-        :picker-options="pickerOptions">
-      </el-date-picker>
-    </el-form-item>
+        <el-form-item label="结束时间">
+          <el-date-picker
+            v-model="end_at"
+            type="datetime"
+            placeholder="选择日期时间"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            align="right"
+            :picker-options="pickerOptions"
+          />
+        </el-form-item>
 
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">搜索</el-button>
-    </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">搜索</el-button>
+        </el-form-item>
 
-    </el-form>
-  </div>
+      </el-form>
+    </div>
 
     <el-table
       border
       :data="tableData"
       highlight-current-row
-      @current-change="handleCurrentChange"
       style="width: 100%"
+      @current-change="handleCurrentChange"
     >
-    <el-table-column
+      <el-table-column
         label="ID"
         prop="id"
         width="100"
         fixed
       />
-    <el-table-column
-      label="管理员信息"
-      prop="admin"
-      width="auto"
-      :formatter="formatter"
-    />
-    <el-table-column
-      label="操作内容"
-      prop="log"
-      width="auto"
-    />
-    <el-table-column
-      label="IP"
-      prop="ip"
-      width="100"
-    />
-    <el-table-column
-      label="请求方法"
-      prop="method"
-      width="80"
-    />
-    <el-table-column
-      label="请求参数"
-      prop="params"
-      width="auto"
-      :formatter="formatter"
-    />
-    <el-table-column
-      label="创建时间"
-      prop="created_at"
-      width="200"
-      fixed="right"
-    />
+      <el-table-column
+        label="管理员信息"
+        prop="admin"
+        width="auto"
+        :formatter="formatter"
+      />
+      <el-table-column
+        label="操作内容"
+        prop="log"
+        width="auto"
+      />
+      <el-table-column
+        label="IP"
+        prop="ip"
+        width="100"
+      />
+      <el-table-column
+        label="请求方法"
+        prop="method"
+        width="80"
+      />
+      <el-table-column
+        label="请求参数"
+        prop="params"
+        width="auto"
+        :formatter="formatter"
+      />
+      <el-table-column
+        label="创建时间"
+        prop="created_at"
+        width="200"
+        fixed="right"
+      />
     </el-table>
     <el-pagination
       layout="prev, pager, next"
@@ -108,29 +110,29 @@ export default {
       dialogLoading: false,
       formInline: {},
       pickerOptions: {
-          disabledDate(time) {
-            return time.getTime() > Date.now();
-          },
-          shortcuts: [{
-            text: '今天',
-            onClick(picker) {
-              picker.$emit('pick', new Date());
-            }
-          }, {
-            text: '昨天',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
-            }
-          }, {
-            text: '一周前',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
-            }
-          }]
+        disabledDate(time) {
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: '今天',
+          onClick(picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: '昨天',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: '一周前',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
       },
       start_at: '',
       end_at: ''
@@ -141,7 +143,7 @@ export default {
   },
   methods: {
     onSubmit() {
-      console.log('submit!');
+      console.log('submit!')
       this.loading = true
       syslog(`?page=${this.page}&per_page=${this.limit}&start_at=${this.start_at}&end_at=${this.end_at}`).then((res) => {
         this.tableData = res.data
@@ -151,7 +153,7 @@ export default {
       })
     },
     handleCurrentChange(val) {
-      this.currentRow = val;
+      this.currentRow = val
     },
     getList() {
       this.loading = true
@@ -162,11 +164,11 @@ export default {
         this.loading = false
       })
     },
-    formatter:function(row,column){
-      //console.log(column)
+    formatter: function(row, column) {
+      // console.log(column)
       switch (column.label) {
         case '管理员信息':
-          return '登录账号:'+row.admin.account+' 名称:'+row.admin.name+' AID:'+row.admin.id
+          return '登录账号:' + row.admin.account + ' 名称:' + row.admin.name + ' AID:' + row.admin.id
         case '请求参数':
           return JSON.stringify(row.params)
       }
