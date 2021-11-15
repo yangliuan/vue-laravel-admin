0c4e37d4d497fbcd6530626245797a5f07feca1a
yangliuan <yangliuancn@foxmail.com>
2021-11-15 16:31:34 +0800

    completed rules page

--- src/views/permission/rules/components/RulesDetail.vue
+++ src/views/permission/rules/components/RulesDetail.vue
@@ -7,31 +7,31 @@
         <el-cascader v-model="postForm.pid" :options="rules_select_menus" :props="{ value: 'id', label: 'name', checkStrictly: true}" clearable />
       </el-form-item>
 
-      <el-form-item label="规则标题" prop="title">
+      <el-form-item label="规则标题" prop="name">
         <el-input v-model="postForm.name" placeholder="规则标题" clearable />
       </el-form-item>
 
-      <el-form-item label="icon" prop="desc">
+      <el-form-item label="icon" prop="icon">
         <el-input v-model="postForm.icon" placeholder="请输入icon" clearable />
       </el-form-item>
 
-      <el-form-item label="gui类型" prop="desc">
+      <el-form-item label="gui类型" prop="gui_type">
         <el-radio v-for="(item,index) in gui_type_radios" :key="index" v-model="postForm.gui_type" :label="item.value">{{ item.label }}</el-radio>
       </el-form-item>
 
-      <el-form-item label="gui规则" prop="desc">
+      <el-form-item label="gui规则" prop="gui_behavior">
         <el-input v-model="postForm.gui_behavior" placeholder="请输入gui规则" clearable />
       </el-form-item>
 
-      <el-form-item label="api方法" prop="desc">
+      <el-form-item label="api方法" prop="api_http_method">
         <el-checkbox v-for="(item,index) in api_http_methods_checkboxs" :key="index" v-model="postForm.api_http_method" :label="item.value">{{ item.label }}</el-checkbox>
       </el-form-item>
 
-      <el-form-item label="api路由" prop="desc">
+      <el-form-item label="api路由" prop="api_behavior">
         <el-input v-model="postForm.api_behavior" placeholder="api路由" clearable />
       </el-form-item>
 
-      <el-form-item label="api路由参数" prop="desc">
+      <el-form-item label="api路由参数" prop="params">
         <el-input v-model="postForm.params" placeholder="api路由参数" clearable />
       </el-form-item>
 
@@ -41,6 +41,10 @@
 
       <el-form-item label="日志状态" prop="is_log">
         <el-radio v-for="(item, index) in log_status_radios" :key="index" v-model="postForm.is_log" :label="item.value">{{ item.label }}</el-radio>
+      </el-form-item>
+
+      <el-form-item label="排序值" prop="sort">
+        <el-input v-model="postForm.sort" placeholder="请输入排序值" oninput="value=value.replace(/[^\d]/g,'')" clearable />
       </el-form-item>
 
       <el-form-item>
@@ -66,7 +70,8 @@ const defaultForm = {
   api_behavior: '',
   params: '',
   status: 1,
-  is_log: 1
+  is_log: 1,
+  sort: 0
 }
 
 export default {
@@ -82,8 +87,17 @@ export default {
       postForm: Object.assign({}, defaultForm),
       loading: false,
       rules: {
-        title: [{ required: true, message: '请输入管理组名称', trigger: 'blur' }, { min: 0, max: 20, message: '长度在0~20之间', trigger: 'blur' }],
-        status: [{ required: true, message: '请选择启用状态', trigger: 'blur' }]
+        pid: [{ required: false, message: '请选择父级规则', trigger: 'blur' }],
+        name: [{ required: true, message: '请输入规则标题', trigger: 'blur' }, { min: 0, max: 100, message: '长度在0~100之间', trigger: 'blur' }],
+        icon: [{ required: false, message: '请输入icon', trigger: 'blur' }, { min: 0, max: 100, message: '长度在0~100之间', trigger: 'blur' }],
+        gui_type: [{ required: true, message: '请选择gui类型', trigger: 'blur' }],
+        gui_behavior: [{ required: true, message: '请输入gui规则', trigger: 'blur' }, { min: 0, max: 255, message: '长度在0~255之间', trigger: 'blur' }],
+        api_http_method: [{ required: true, message: '请选择api请求方法', trigger: 'blur' }],
+        api_behavior: [{ required: true, message: '请输入api路由', trigger: 'blur' }, { min: 0, max: 255, message: '长度在0~255之间', trigger: 'blur' }],
+        params: [{ required: true, message: '请输入api路由参数', trigger: 'blur' }, { min: 0, max: 255, message: '长度在0~255之间', trigger: 'blur' }],
+        status: [{ required: true, message: '请选择启用状态', trigger: 'blur' }],
+        is_log: [{ required: true, message: '请选择日志状态', trigger: 'blur' }],
+        sort: [{ required: true, message: '请输入排序值', trigger: 'blur' }]
       },
       gui_type_radios: [
         { label: '主菜单', value: 1 },
@@ -130,6 +144,9 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
+          const key = this.postForm.pid.length - 1
+          this.postForm.pid = this.postForm.pid[key]
+          this.postForm.api_http_method = this.postForm.api_http_method.join()
           this.loading = true
           if (this.isEdit === false) {
             storeRules(this.postForm).then(response => {
